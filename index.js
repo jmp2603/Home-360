1aebf66f9937baea9b4adf60214dfd1d2fb10466
jaimin <jaimin21@groovyweb.co>
2024-05-20 18:39:58 +0530

    🔨 : Android Build issue fix

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 /**
  * @format
  */
-
-import {AppRegistry} from 'react-native';
-import {name as appName} from './app.json';
-import messaging from '@react-native-firebase/messaging';
-import {showNotification} from './app/components/Services/NotificationHelper';
-const App = require('./app/Entrypoint').default;
+import "react-native-gesture-handler";
+import { AppRegistry } from "react-native";
+import { name as appName } from "./app.json";
+import messaging from "@react-native-firebase/messaging";
+import { showNotification } from "./app/components/Services/NotificationHelper";
+const App = require("./app/Entrypoint").default;
 // Register background handler
-messaging().setBackgroundMessageHandler(async remoteMessage => {
-  console.log('Message handled in the background!', remoteMessage);
+messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+  console.log("Message handled in the background!", remoteMessage);
   showNotification(remoteMessage, false);
 });
 
